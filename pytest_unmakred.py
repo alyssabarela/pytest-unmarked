ae1b16e31758a39152014a36bde0aa8509c00c5e
Alyssa <alyssa.barela17@gmail.com>
2015-02-18 12:25:33 -0700

    trying to figure out why module isnt recognized

--- pytest_unmakred.py
+++ pytest_unmakred.py
@@ -9,34 +9,3 @@ def pytest_addoption(parser):
                      help="Run unmarked tests.")
 
 
-def pytest_cmdline_main(config):
-    if config.option.markers:
-        config.do_configure()
-        tw = py.io.TerminalWriter()
-        for line in config.getini("markers"):
-            name, rest = line.split(":", 1)
-            tw.write("@pytest.mark.%s:" % name, bold=True)
-            tw.line(rest)
-            tw.line()
-        config.do_unconfigure()
-        return 0
-pytest_cmdline_main.tryfirst = True
-
-
-def pytest_collection_modifyitems(items, config):
-    matchexpr = config.option.markers
-    if not matchexpr:
-        return
-
-    remaining = []
-    deselected = []
-    for colitem in items:
-        if matchexpr:
-            if matchmark(colitem, matchexpr):
-                deselected.append(colitem)
-                continue
-        remaining.append(colitem)
-
-    if deselected:
-        config.hook.pytest_deselected(items=deselected)
-        items[:] = remaining
