0efbb40fd4b6c92b97183c848e12ebd51c0acc7d
Alyssa Barela <abarela@cradlepoint.com>
2015-05-08 14:53:51 -0600

    version bump

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ def read(fname):
 
 
 setup(name='pytest-unmarked',
-      version='1.1',
+      version='1.2',
       description='Run only unmarked tests',
       author='Alyssa Barela',
       author_email='alyssa.barela17@gmail.com',
